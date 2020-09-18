db640d4a93c51f80886b87e2bb0cb68483ea67ac
unknown <shamolmojumder14@gmail.com>
2020-09-19 01:01:36 +0600

    Create Shop Component, load data and set state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import './App.css';
+import Header from './componentes/Header/Header';
+import Shop from './componentes/Shop/Shop';
+
 
 function App() {
   return (
     <div >
-
+      <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
