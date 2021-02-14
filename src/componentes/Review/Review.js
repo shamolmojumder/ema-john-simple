f43103a9b48b5bd1db9220e3f9acd1cea5a149cf
shamolmojumder <shamolmojumder14@gmail.com>
2021-02-15 02:07:08 +0600

    8.00

--- src/componentes/Review/Review.js
+++ src/componentes/Review/Review.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
+import { fakedata } from '../data/fakeData';
 
 const Review = () => {
     const {cart,setCart} = ([])    
@@ -7,7 +8,9 @@ const Review = () => {
         // cart
         const savedCart=getDatabaseCart();
         const productKeys=Object.keys(savedCart)
-        const counts=productKeys.map(key => savedCart[key] )
+        const counts=productKeys.map(key => {
+          const product = fakedata
+        })
         console.log(counts);
     })
     return (
