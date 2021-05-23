d3c59f77550387ed2adc18e47288e726d43b4933
shamolmojumder <shamolmojumder14@gmail.com>
2021-05-23 10:40:15 +0600

    minute-1.29s

--- src/componentes/NotFound/NotFound.js
+++ src/componentes/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
+        <div style={{textAlign:'center'}}>
             <h1>Page Not found</h1>
             <h3>404 error</h3>
+            <Link to="/">Go back to Shop</Link>
         </div>
     );
 };
