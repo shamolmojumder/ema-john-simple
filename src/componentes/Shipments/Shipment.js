04ad6d4b16259d26d1161a90c4926066690f096e
shamolmojumder <shamolmojumder14@gmail.com>
2021-05-28 13:32:11 +0600

    create shipment form using React hook form

--- src/componentes/Shipments/Shipment.js
+++ src/componentes/Shipments/Shipment.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import { UserContext } from '../../App';
+import './Shipment.css';
 
 const Shipment = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-    return (
-        
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input defaultValue="test" {...register("example")} />
-          <input {...register("exampleRequired", { required: true })} />
-          {errors.exampleRequired && <span>This field is required</span>}  
-          <input type="submit" />
-        </form>
-      );
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [loggedInUser,setLoggedInUser]=useContext(UserContext)
+  const onSubmit = data => {
+    console.log("form submmited",data)
+  };
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    
+    <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue={loggedInUser.name} {...register("name", { required: true })} placeholder="Your Name" />
+      {errors.name && <span className="error">This name is required</span>}  
+
+      <input defaultValue={loggedInUser.email} {...register("email", { required: true })} placeholder="Your email"/>
+      {errors.email && <span className="error">This email is required</span>}  
+      
+      <input  {...register("address", { required: true })} placeholder="Your address"/>
+      {errors.address && <span className="error">This address is required</span>}  
+      
+      <input  {...register("phone", { required: true })} placeholder="Your phone"/>
+      {errors.phone && <span className="error"> phone is required</span>}  
+      
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
