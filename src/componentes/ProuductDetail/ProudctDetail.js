10d9ebd021f8bcbef396e943e24c87266844281f
shamolmojumder <shamolmojumder14@gmail.com>
2021-09-27 01:24:46 +0600

    48-4 load data from the server

--- src/componentes/ProuductDetail/ProudctDetail.js
+++ src/componentes/ProuductDetail/ProudctDetail.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 const ProudctDetail = () => {
     const {productKey}=useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
- //   console.log(product);
+    const [product,setProduct]=useState({})
+    useEffect(()=>{
+        fetch('http://localhost:5000/product/'+productKey)
+        .then(res=>res.json())
+        .then(data=>setProduct(data))
+    },[productKey])
     
     return (
         <div>
--- src/componentes/Shop/Shop.js
+++ src/componentes/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import fakeData from '../../fakeData';
 import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
@@ -8,25 +7,25 @@ import './Shop.css'
 
 
 const Shop = () => {
-    const first10 = fakeData.slice(0, 10)
-    const [products, setProducts] = useState(first10)
+    const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
-
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     useEffect(()=>{
         const savedCart=getDatabaseCart();
-        // console.log(Array.isArray("savedCart"+savedCart));
-      //   console.log(Array.isArray(savedCart));
-        const productKeys=Object.keys(savedCart)
-        //console.log(Array.isArray('productKeys' + productKeys));
-            //  console.log(Array.isArray(productKeys));
-             
-        const previousCart=productKeys.map(existingKey =>{
-            const product=fakeData.find(pd=>pd.key=== existingKey);
-            product.quantity=savedCart[existingKey];
-            return product;
-        })
-        setCart(previousCart)
-    },[])
+        const productKeys=Object.keys(savedCart);
+        if (products.length) {
+            const previousCart=productKeys.map(existingKey =>{
+                const product=products.find(pd=>pd.key=== existingKey);
+                product.quantity=savedCart[existingKey];
+                return product;
+            })
+            setCart(previousCart);
+        }
+    },[products])
     
 
     const handleAddProduct = (product) => {
