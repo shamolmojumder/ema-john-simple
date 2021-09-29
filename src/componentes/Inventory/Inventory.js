cad4ef44181e5e24522bf3424b6aae9d1687a39e
shamolmojumder <shamolmojumder14@gmail.com>
2021-09-30 01:10:14 +0600

    48-6 Handle Inventory and how to store image on server

--- src/componentes/Inventory/Inventory.js
+++ src/componentes/Inventory/Inventory.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import fakeData from '../../fakeData'
 
 const Inventory = () => {
     const handleAddProduct=()=>{
+        const product={};
         fetch('http://localhost:5000/addProduct',{
             method:'POST',
             headers:{
                 'Content-type': 'application/json',
             },
-            body:JSON.stringify(fakeData)
+            body:JSON.stringify(product)
         })        
     }
     return (
         <div>
+            <form action="">
+            <p><span>Name</span><input type="text" /></p>
+            <p><span>Price</span><input type="text" /></p>
+            <p><span>Quantity</span><input type="text" /></p>
+            <p><span>Product image</span><input type="file"/></p>
            <button onClick={handleAddProduct}>Add product</button>
+            </form>
         </div>
     );
 };
